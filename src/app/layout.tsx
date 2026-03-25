4b5c4406898a51020fbc884e8f22f9c7b842f5d6
rectanglehihat <rectanglehihat@gmail.com>
2026-03-25 16:53:36 +0900

    chore: metadata title 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,33 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "도안 - 뜨개질 도안 에디터",
-  description: "간단한 입력으로 뜨개질 도안을 시각적으로 생성하고 PDF로 출력할 수 있는 웹 기반 도안 에디터",
+	title: '도안 - 뜨개 도안 에디터',
+	description: '간단한 입력으로 뜨개질 도안을 시각적으로 생성하고 PDF로 출력할 수 있는 웹 기반 도안 에디터',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+		>
+			<body className="min-h-full flex flex-col">{children}</body>
+		</html>
+	);
 }
